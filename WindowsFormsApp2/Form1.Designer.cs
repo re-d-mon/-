4444eb7343b172bb9c5080b158cf0745dcb444b2
kirill-kiron@mail.ru <kirill-kiron@mail.ru>
2021-03-12 04:13:44 +0800

    доделал поиск длины окружности

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class калькулятор
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(калькулятор));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Name = "panel1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Close
+            // 
+            resources.ApplyResources(this.Close, "Close");
+            this.Close.BackColor = System.Drawing.Color.OrangeRed;
+            this.Close.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Close.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Close.Name = "Close";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            resources.GetString("textBox1.AutoCompleteCustomSource")});
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Aktiv);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_load);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            resources.GetString("textBox2.AutoCompleteCustomSource")});
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.UseWaitCursor = true;
+            // 
+            // калькулятор
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "калькулятор";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.калькулятор_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class калькулятор : Form
+    {
+        public калькулятор()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void калькулятор_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(21, 30, 51);
+            this.BackColor = Color.FromArgb(46, 52, 72);
+            Close.ForeColor = Color.FromArgb(26, 26, 26);
+            label1.ForeColor = Color.FromArgb(154, 164, 176);
+            button1.BackColor = Color.FromArgb(21, 30, 51);
+            button1.ForeColor = Color.FromArgb(40, 115, 198);
+            button2.BackColor = Color.FromArgb(21, 30, 51);
+            button2.ForeColor = Color.FromArgb(40, 115, 198);
+            panel2.BackColor = Color.FromArgb(46, 52, 72);
+            textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            textBox1.Text = "R=";
+            textBox1.ForeColor = Color.Gray;
+        }
+
+
+
+        private void Close_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void textBox1_load(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "R=";
+                textBox1.ForeColor = Color.Gray;
+            } 
+        }
+
+        private void textBox1_Aktiv(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "R=")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+    }
+}
